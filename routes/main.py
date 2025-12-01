4352812efd12f9d92ea45a64ce47118e04be789e
Oleksii Sterlikov <Oleksii.Sterlikov@gmail.com>
2025-12-01 14:59:53 +0200

    Create route for readed lang name. Needed modifaire. Coment/

--- routes/main.py
+++ routes/main.py
@@ -1,13 +1,35 @@
 from app import app
-from flask import render_template
+from flask import render_template, send_from_directory, abort
+from werkzeug.utils import safe_join
 from services.fswalker import read_folder, print_tree
-import json
+import os
 
 
 @app.route("/")
-def hello_world():
+def main():
     data = read_folder('./static/books')
     # print(json.dumps(data, indent=2, ensure_ascii=False))
     # hello = "Hello! This is the Book Reader!"
     # print_tree(read_folder('./static/books')) # Uncomment to view the folder structure in the console
     return render_template("tree.html", tree=data)
+
+
+#@app.route("/book/<path:file_path>")
+#def serve_book(file_path):
+#    base_dir = os.path.join(app.static_folder, "books")
+#    abs_path = safe_join(base_dir, file_path)
+#
+#    if abs_path is None:
+#        return abort(404)
+#
+#    # Додаємо підтримку довгих шляхів Windows
+#    if os.name == "nt":
+#        abs_path = "\\\\?\\" + os.path.abspath(abs_path)
+#
+#    if not os.path.isfile(abs_path):
+#        return abort(404)
+#
+#    directory = os.path.dirname(abs_path)
+#    filename = os.path.basename(abs_path)
+#
+#    return send_from_directory(directory, filename)
